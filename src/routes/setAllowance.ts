46a27088d9afc2f0710d422dcb360104b5ca2a46
ankitpal1029 <ankitkumarpal27@gmail.com>
2023-02-05 09:44:36 +0530

    reset allowance works

--- src/routes/setAllowance.ts
+++ src/routes/setAllowance.ts
@@ -1,21 +1,32 @@
 import { sign } from "crypto";
 import { BigNumber, ethers, Wallet } from "ethers";
 import express from "express";
+import bodyParser from "body-parser";
 
 const setAllowanceRoute = express.Router();
 
-setAllowanceRoute.get("/clearAllowance", async (req, res) => {
+const jsonParser = bodyParser.json();
+
+setAllowanceRoute.post("/clearAllowance", jsonParser, async (req, res) => {
   req;
   res;
-  const signedTransaction = req.query.signedTransaction as string;
+  const signedTransactions = req.body.signedTransactions;
+  // const signedTransaction = req.query.signedTransaction as string;
   const alchemyURI = process.env.ALCHEMY_URL;
   const provider = new ethers.providers.JsonRpcProvider(alchemyURI);
-  const tx = await provider.sendTransaction(signedTransaction);
-  const receipt = await tx.wait();
-  console.log(receipt);
 
-  console.log("sdfksdj");
-  res.json("yo");
+  for (let i = 0; i < signedTransactions.length; i++) {
+    try {
+      const tx = await provider.sendTransaction(signedTransactions[i]);
+      const receipt = await tx.wait();
+      console.log(receipt);
+    } catch (err) {
+      res.json("failed");
+      console.log(err);
+    }
+  }
+
+  res.json("sucess");
 });
 
 // setAllowanceRoute.get("/populate-tx", async (req, res) => {
