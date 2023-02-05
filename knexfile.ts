e29a7587a028a250c599c5241309279b9d882933
ankitpal1029 <ankitkumarpal27@gmail.com>
2023-02-05 11:32:52 +0530

    knex env

--- knexfile.ts
+++ knexfile.ts
@@ -1,10 +1,14 @@
 import type { Knex } from "knex";
+import dotenv from "dotenv";
+dotenv.config();
 
 const HOST = process.env.DB_HOST as string;
 const PORT = parseInt(process.env.DB_PORT as string);
 const USER = process.env.DB_USER_ as string;
 const PASSWORD = process.env.DB_PASSWORD_ as string;
 const DATABASE = process.env.DB_DATABASE_ as string;
+
+console.log(HOST, PORT, USER, PASSWORD, DATABASE);
 
 const config: { [key: string]: Knex.Config } = {
   development: {
@@ -13,10 +17,37 @@ const config: { [key: string]: Knex.Config } = {
       host: HOST,
       port: PORT,
       // user: USER,
-      user: "root",
+      user: USER,
       // password: PASSWORD,
-      password: "secret",
-      database: "approval_events",
+      password: PASSWORD,
+      database: DATABASE,
+      ssl: true,
+    },
+  },
+  staging: {
+    client: "pg",
+    connection: {
+      host: HOST,
+      port: PORT,
+      // user: USER,
+      user: USER,
+      // password: PASSWORD,
+      password: PASSWORD,
+      database: DATABASE,
+      ssl: true,
+    },
+  },
+  production: {
+    client: "pg",
+    connection: {
+      host: HOST,
+      port: PORT,
+      // user: USER,
+      user: USER,
+      // password: PASSWORD,
+      password: PASSWORD,
+      database: DATABASE,
+      ssl: true,
     },
   },
 };
